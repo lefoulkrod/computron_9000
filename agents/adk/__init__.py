eb2240f10f9e222e43dfb04e069f2e1c7eef897e
Larry Foulkrod <lefoulkrod@gmail.com>
2025-07-04 10:36:08 -0500

    fixed up some import/export

--- agents/adk/__init__.py
+++ agents/adk/__init__.py
@@ -1,6 +1,9 @@
 # This file marks the adk directory as a Python package after moving code.
 
+from .message_handler import handle_user_message
 from config import load_config
+
+__all__ = ['handle_user_message']
 
 def get_adk_model() -> str:
     """
