0c65090603679b8eaa4dbd88687a6d34ebb8ccd2
Larry Foulkrod <lefoulkrod@gmail.com>
2025-07-15 21:01:01 -0500

    added some typings to cache.py module

--- utils/cache.py
+++ utils/cache.py
@@ -1,10 +1,13 @@
 import functools
 import logging
 from collections.abc import Awaitable, Callable
-from typing import Any
+from typing import Any, ParamSpec, TypeVar
 
 import cachetools
 from cachetools.keys import hashkey
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 logger = logging.getLogger(__name__)
 
@@ -14,8 +17,10 @@ _cache_registry: dict[str, cachetools.LRUCache[Any, Any]] = {}
 
 def async_lru_cache(
     maxsize: int = 10,
-) -> Callable[[Callable[..., Awaitable[Any]]], Callable[..., Awaitable[Any]]]:
-    """Decorator to cache async function results using cachetools. If no cache is provided, creates one per function.
+) -> Callable[[Callable[P, Awaitable[R]]], Callable[P, Awaitable[R]]]:
+    """Decorator to cache async function results using cachetools.
+
+    If no cache is provided, creates one per function.
 
     Args:
         maxsize (int): Maximum size of the LRU cache. Default is 10.
@@ -25,19 +30,19 @@ def async_lru_cache(
 
     """
 
-    def decorator(func: Callable[..., Awaitable[Any]]) -> Callable[..., Awaitable[Any]]:
+    def decorator(func: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
         cache_key = f"{func.__module__}.{func.__qualname__}"
         if cache_key not in _cache_registry:
             _cache_registry[cache_key] = cachetools.LRUCache(maxsize=maxsize)
         cache = _cache_registry[cache_key]
 
         @functools.wraps(func)
-        async def wrapper(*args: Any, **kwargs: Any) -> Any:
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             key = hashkey(*args, **kwargs)
             if key in cache:
-                logger.debug(f"Cache hit for {cache_key} key: {key}")
+                logger.debug("Cache hit for %s key: %r", cache_key, key)
                 return cache[key]
-            logger.debug(f"Cache miss for {cache_key} key: {key}")
+            logger.debug("Cache miss for %s key: %r", cache_key, key)
             result = await func(*args, **kwargs)
             cache[key] = result
             return result
