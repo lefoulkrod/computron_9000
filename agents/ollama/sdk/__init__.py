44ff0ff257a91dc3718e86eb7324fcf850cbc953
Larry Foulkrod <lefoulkrod@gmail.com>
2025-09-04 21:22:06 -0500

    lint stuff

--- agents/ollama/sdk/__init__.py
+++ agents/ollama/sdk/__init__.py
@@ -1,8 +1,8 @@
 from agents.types import Agent
 
-from .run_agent_tools import make_run_agent_as_tool_function
 from .llm_runtime_stats import LLMRuntimeStats, llm_runtime_stats
 from .logging_callbacks import make_log_after_model_call, make_log_before_model_call
+from .run_agent_tools import make_run_agent_as_tool_function
 from .tool_loop import run_tool_call_loop
 
 __all__ = [
